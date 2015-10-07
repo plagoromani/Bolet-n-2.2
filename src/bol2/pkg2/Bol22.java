94684ed3a79782baeca38c07d734e73cd022723d
plagoromani <plagoromani@atanasoff09.danielcastelao.org>
2015-10-07 10:48:22 +0200

    Final

---
+++ src/bol2/pkg2/Bol22.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol2.pkg2;
+
+import java.util.Scanner;
+
+
+public class Bol22 {
+
+  
+    public static void main(String[] args) {
+        float lado ;
+        Scanner obx = new Scanner(System.in);
+        System.out.println("teclea lado :");
+        lado = obx.nextFloat();
+        System.out.println("area =" +lado*lado);
+        
+        
+        
+    }
+    
+}
